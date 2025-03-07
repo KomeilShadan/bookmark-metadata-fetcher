ce59f6666dd653fb848004cd9fa35c614941234d
Komeil <kshadannejad@gmail.com>
2025-03-07 15:44:18 +0330

    update bookmark tests

--- tests/Feature/BookmarkTest.php
+++ tests/Feature/BookmarkTest.php
@@ -2,8 +2,11 @@
 
 namespace Tests\Feature;
 
+use App\Jobs\FetchMetadataJob;
 use App\Models\Bookmark;
 use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Support\Facades\Queue;
+use Illuminate\Validation\ValidationException;
 use Symfony\Component\HttpFoundation\Response;
 use Tests\TestCase;
 
@@ -11,11 +14,24 @@ class BookmarkTest extends TestCase
 {
     use RefreshDatabase;
 
-    public function test_retrieve_all_bookmarks(): void
+    public function test_bookmark_auth_middleware(): void
     {
-        Bookmark::factory(5)->create();
+        $this->withoutExceptionHandling();
 
         $response = $this->getJson('/api/bookmarks');
+        $response->assertStatus(Response::HTTP_UNAUTHORIZED);
+
+        $response = $this->postJson('/api/bookmarks', ['url' => 'malware.com']);
+        $response->assertStatus(Response::HTTP_UNAUTHORIZED);
+    }
+
+    public function test_retrieve_all_bookmarks(): void
+    {
+        $this->withoutExceptionHandling();
+
+        Bookmark::factory(5)->create();
+
+        $response = $this->getJson('/api/bookmarks', ['Authorization' => config('api.api_token')]);
 
         $response->assertStatus(Response::HTTP_OK)
                  ->assertJsonStructure(['data' => [['id', 'url', 'title', 'description', 'created_at']]]);
@@ -23,7 +39,9 @@ class BookmarkTest extends TestCase
 
     public function test_retrieve_empty_bookmarks(): void
     {
-        $response = $this->getJson('/api/bookmarks');
+        $this->withoutExceptionHandling();
+
+        $response = $this->getJson('/api/bookmarks', ['Authorization' => config('api.api_token')]);
 
         $response->assertStatus(Response::HTTP_OK)
                  ->assertJson(['data' => []]);
@@ -31,25 +49,38 @@ class BookmarkTest extends TestCase
 
     public function test_store_bookmark_with_valid_url(): void
     {
+        $this->withoutExceptionHandling();
+        Queue::fake();
+
         $validUrl = 'https://google.com';
         $response = $this->postJson('/api/bookmarks', [
             'url' => $validUrl,
+        ],
+        [
+            'Authorization' => config('api.api_token')
         ]);
 
         $response->assertStatus(Response::HTTP_CREATED)
                  ->assertJson([
-                     'message' => 'Bookmark submitted successfully!',
+                     'message' => 'Bookmark submitted successfully',
                  ]);
 
         $this->assertDatabaseHas('bookmarks', [
             'url' => $validUrl,
         ]);
+        Queue::assertPushed(FetchMetadataJob::class);
     }
 
     public function test_store_bookmark_with_invalid_url(): void
     {
+        $this->withoutExceptionHandling();
+        $this->expectException(ValidationException::class);
+
         $response = $this->postJson('/api/bookmarks', [
             'url' => 'https://invalid-url-123456.com',
+        ],
+        [
+            'Authorization' => config('api.api_token')
         ]);
 
         $response->assertStatus(Response::HTTP_UNPROCESSABLE_ENTITY)
@@ -58,7 +89,10 @@ class BookmarkTest extends TestCase
 
     public function test_store_bookmark_with_missing_url(): void
     {
-        $response = $this->postJson('/api/bookmarks', []);
+        $this->withoutExceptionHandling();
+        $this->expectException(ValidationException::class);
+
+        $response = $this->postJson('/api/bookmarks', [], ['Authorization' => config('api.api_token')]);
 
         $response->assertStatus(Response::HTTP_UNPROCESSABLE_ENTITY)
                  ->assertJsonValidationErrors(['url']);
